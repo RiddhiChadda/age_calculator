d89c568f884e9d39590c6b23cddf90d7d9ade8c1
Riddhi <riddhi.chadda@gmail.com>
2026-01-06 15:30:00 +0000

    Age (years) logic added

--- age_calc.py
+++ age_calc.py
@@ -1,18 +1,22 @@
-import datetime 
+import datetime
+
 today = datetime.datetime.now()
 
 current_year = today.year
 current_month = today.month
 current_day = today.day
 
-year_born = int(input("What year were you born in ? "))
-month_born = int(input("What month were you born in ? "))
-date_born = int(input("What date were you born in ? "))
+year_born = int(input("What year were you born in? "))
+month_born = int(input("What month were you born in? "))
+date_born = int(input("What day were you born on? "))
 
-print(current_year)
-print(current_month)
-print(current_day)
+age = current_year - year_born
 
-print(year_born)
-print(month_born)
-print(date_born)
+if current_month < month_born:
+    age = age - 1
+
+elif current_month == month_born:
+    if current_day < date_born:
+        age = age - 1
+
+print("Your age is:", age)

fccb2daff97a2a382eca621af748bfcbbc72a0b0
Riddhi <riddhi.chadda@gmail.com>
2026-01-06 10:00:00 +0000

    Retrieving today's Y/M/D in 3 var

--- age_calc.py
+++ age_calc.py
@@ -1,6 +1,18 @@
+import datetime 
+today = datetime.datetime.now()
+
+current_year = today.year
+current_month = today.month
+current_day = today.day
+
 year_born = int(input("What year were you born in ? "))
 month_born = int(input("What month were you born in ? "))
 date_born = int(input("What date were you born in ? "))
+
+print(current_year)
+print(current_month)
+print(current_day)
+
 print(year_born)
 print(month_born)
 print(date_born)
